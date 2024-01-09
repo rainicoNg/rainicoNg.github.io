0861938314335513cbeccdc55aeebf23171117e1
rainicoying <rainicong@magicubedu.com>
2024-01-09 23:28:07 +0800

    added hash

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,16 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 const routes = [
 	{
+		path: "/",
+		component: () => import("./views/Home.vue")
+	},
+	{
 		path: "/rainicong.github.io/",
     name: "Home",
     component: () => import("./views/Home.vue")
 	},
 	{
-		path: "/rainicong.github.io/lucky-draw",
+		path: "/rainicong.github.io/#/lucky-draw",
 		name: "Lucky Draw",
 		component: () => import("./views/LuckyDraw.vue"),
 	}

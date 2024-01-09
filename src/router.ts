2dcb354d5bd6d686efed75df88b14b4c68519c0f
rainicoying <rainicong@magicubedu.com>
2024-01-09 23:36:19 +0800

    built

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
 	{
+		path: "/",
+		component: () => import("./views/LuckyDraw.vue"),
+	},
+	{
+		path: "/rainicong.github.io/",
+		name: "Home",
+		component: () => import("./views/LuckyDraw.vue"),
+	},
+	{
 		path: "/rainicong.github.io/lucky-draw",
 		name: "Lucky Draw",
 		component: () => import("./views/LuckyDraw.vue"),
@@ -8,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory("/rainicong.github.io/"),
 	routes,
 });
 export default router;
